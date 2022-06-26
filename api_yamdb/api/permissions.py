ad5a2fb79f25e9c464a4905e454dccd4cce4b3bb
Даниил <KostromDan@gmail.com>
2022-06-27 01:46:16 +0300

    Small fixes.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 
 class IsAdmin(IsAuthenticated):
     def has_permission(self, request, view):
-        return (super(IsAdmin, self).has_permission(request, view)
+        return (super().has_permission(request, view)
                 and (request.user.is_staff
                      or request.user.is_superuser
                      or request.user.role == 'admin'))
@@ -12,4 +12,4 @@ class IsAdmin(IsAuthenticated):
 class IsAdminOrReadOnly(IsAdmin):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
-                or super(IsAdminOrReadOnly, self).has_permission(request, view))
+                or super().has_permission(request, view))
